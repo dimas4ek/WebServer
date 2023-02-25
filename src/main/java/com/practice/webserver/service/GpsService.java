ad053d0cb6f352c623b28f98e12b5c9784a13ac8
Dmitry Gulyaev <dimas4ek@gmail.com>
2023-02-25 19:21:31 +0500

    add led control

--- src/main/java/com/practice/webserver/service/GpsService.java
+++ src/main/java/com/practice/webserver/service/GpsService.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Socket;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 @Service
 public class GpsService {
@@ -15,19 +17,38 @@ public class GpsService {
 
     public String getLocation() {
 
+        HttpURLConnection connection = null;
         try {
-            Socket socket = new Socket(ESP8266_IP, ESP8266_PORT);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String inputLine;
-            StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+            connection = (HttpURLConnection) new URL("http://" + ESP8266_IP + ":" + ESP8266_PORT + "/").openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setUseCaches(false);
+            connection.setAllowUserInteraction(false);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.connect();
+
+            int status = connection.getResponseCode();
+            if (status == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                return response.toString();
+            } else {
+                throw new RuntimeException("Failed : HTTP error code : " + connection.getResponseCode());
             }
-            in.close();
-            socket.close();
-            return content.toString();
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
+
     }
 }
